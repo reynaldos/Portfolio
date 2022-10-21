9c7708b674919e929d327a631c16b45b9a6f7490
reynaldos <reyjr98@gmail.com>
2022-10-20 22:18:14 -0400

    fonts added

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BuildThemeProvider, BuildStyles} from './ThemeContext';
+import { ThemeProvider } from 'styled-components';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(  
+    <BuildThemeProvider>
+        <ThemeProvider theme={BuildStyles}>
+            <App />
+        </ThemeProvider>
+    </BuildThemeProvider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
