dc45f042fad7490d1fa6bb7abf4f44c5d63e9b27
reynaldos <reyjr98@gmail.com>
2022-10-24 13:05:34 -0400

    side rails build complete

---
+++ src/components/data.js
@@ -0,0 +1,20 @@
+
+export const Data = {
+    nav: [
+        { title: 'work' , link: './' },
+        { title: 'about' , link: './' },
+        { title: 'resume' , link: './' },
+    ],
+    socials:[
+        {name: 'github', icon: './', link: './'},
+        {name: 'instagram', icon: './', link: './'},
+        {name: 'twitter', icon: './', link: './'},
+        {name: 'linkedin', icon: './', link: './'},
+        {name: 'phone', icon: './icons/phone.svg', link: './'},
+
+
+
+    ]
+
+
+}
---
+++ src/components/siderails.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Data } from './data';
+import { SocialIcons } from './socials';
+
+
+export const SideRails = ({currentBuild}) => {
+  return (
+    <>
+      <Container side={'left'} >
+        <Wrap>
+            <Holder>
+                {Data.socials.map((item, index)=>{
+                    return <IconWrap key={index}>
+                            <SocialIcons 
+                                currentBuild={currentBuild}
+                                index={index}/>
+                </IconWrap>
+                })}
+            </Holder>
+
+            <Line currentBuild={currentBuild}/>
+        </Wrap>
+    </Container>
+
+
+    {/* ////////////////////////////////////  */}
+      <Container side={'right'}  >
+        <Wrap>
+            <EmailWrap>
+                <Email currentBuild={currentBuild}>rey.sanchez.dev@gmail.com</Email>
+            </EmailWrap>
+
+            <Line currentBuild={currentBuild}/>
+        </Wrap>
+    </Container>
+    
+     </>
+  )
+}
+
+const Container = styled.section`
+    /* background-color: aliceblue; */
+    position: fixed;
+    bottom: 0;
+    margin: 0 1rem;
+    width: 48px;
+    height: calc(64px * 5);
+    right: ${props => props.side === 'right' ? '0' : ''};
+    left: ${props => props.side === 'left' ? '0' : ''};
+    transition:  bottom  .5s ease;
+
+    @media screen and (max-width: ${props => props.theme.breakpoint.sm}){
+       bottom: -100%;
+    } 
+
+     @media screen and (min-width: ${props => props.theme.breakpoint.xl}){
+       left: ${props => props.side === 'left' ? 'calc(50% - 675px)' : ''};
+       right: ${props => props.side === 'right' ? 'calc(50% - 675px )' : ''};
+       transform:  ${props => props.side === 'left' ? 'translate(-50%)' : 'translate(50%)'};
+
+
+    } 
+`
+
+const Wrap = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+`
+
+
+const Holder = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content:center;
+    align-items: center;
+`
+const IconWrap = styled.div`
+    /* outline: 1px solid black; */
+    height: 48px;
+    margin-bottom: .5rem;
+    aspect-ratio: 1 / 1;
+    display: grid;
+    place-items: center;
+
+
+`
+
+const Line = styled.div`
+    height: 10%;
+    width: 4px;
+    border-radius: 4px;
+    background-color:  ${props => props.theme[props.currentBuild].accent};
+    transition: background-color ${props => props.theme.transitionStyleBottom};
+`
+
+const EmailWrap = styled.div`
+    width: 100%;
+   
+`
+
+const Email = styled.h6`
+    color:  ${props => props.theme[props.currentBuild].accent};
+    transition: color ${props => props.theme.transitionStyleBottom};
+    /* transform-origin: 0 100%; */
+    text-align: right;
+    transform: translateY(50%) rotate(90deg);
+`
